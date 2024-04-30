889a49d62274cea9bf5297fddc98cdee6ff40b2f
Arda Macbook Pro <ardamacbookpro@192.168.1.100>
2024-04-30 18:19:39 +0300

    added new users

--- functions/check-email.js
+++ functions/check-email.js
@@ -2,8 +2,13 @@ exports.handler = async (event) => {
     const allowedEmails = [
         'ardaakkiz@gmail.com',
         'baran.ozer.yuce@gmail.com',
-        'hello@asinbaba.com'
-    ]; 
+        'hello@asinbaba.com',
+        "gulistanagcal@hotmail.com",
+        "emrahkeles0604@gmail.com",
+        "kahyeogluf@gmail.com",
+        "emrealtan1525@gmail.com",
+        "serdar_tur@hotmail.com"
+    ]
     const userEmail = event.queryStringParameters.email;
 
     const isAllowed = allowedEmails.includes(userEmail);
